5f6c67bd562516589e121a9f1cefc03a1c5d6f81
leo1in88 <tm180887@gmail.com>
2026-04-23 20:54:04 -0500

    Change body font from 'Noto Sans' to 'JetBrains Mono'

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Noto Sans",
+        body: "JetBrains Mono",
         code: "IBM Plex Mono",
       },
       colors: {
